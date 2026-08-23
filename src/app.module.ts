e0e8489781fb9e1f8024f6c53c5a700b142d1c97
baobacluc3 <baobacluc3@gmail.com>
2026-08-23 20:25:57 +0700

    update

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,6 +6,7 @@ import { WalletModule } from './wallet/wallet.module';
 import { TransactionModule } from './transaction/transaction.module';
 import { TransferModule } from './transfer/transfer.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
@@ -19,6 +20,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
     WalletModule,
     TransactionModule,
     TransferModule,
+    AuthModule,
   ],
   controllers: [AppController],
   providers: [AppService],
